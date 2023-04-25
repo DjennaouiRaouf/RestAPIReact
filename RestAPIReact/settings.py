a7bdd7051c2dce4c448ab1ccf531b64c62f38cc9
djennaoui-raouf <djennaoui.raouf@gmail.com>
2023-04-25 23:24:58 +0200

    First Commit

--- RestAPIReact/settings.py
+++ RestAPIReact/settings.py
@@ -25,13 +25,13 @@ SECRET_KEY = 'django-insecure-5%viv433r8up(t_7rqrb49vtflw#-#=vhsc1$h%$!8y_8elea(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
-
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,10 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
 os.path.join(BASE_DIR, "media"),]
 
 # Default primary key field type
