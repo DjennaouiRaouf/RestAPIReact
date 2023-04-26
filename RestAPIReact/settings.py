2a4d1411588038c2b8b83df7156005c858d28366
djennaoui-raouf <djennaoui.raouf@gmail.com>
2023-04-26 17:30:42 +0200

    First Commit

--- RestAPIReact/settings.py
+++ RestAPIReact/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5%viv433r8up(t_7rqrb49vtflw#-#=vhsc1$h%$!8y_8elea(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -124,8 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'frontend', 'build', 'staticfiles')
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
 os.path.join(BASE_DIR, "media"),]
 
