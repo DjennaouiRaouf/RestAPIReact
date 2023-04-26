7688602e5f6e71026e00f23ebe0ee5841636579c
djennaoui-raouf <djennaoui.raouf@gmail.com>
2023-04-27 01:00:57 +0200

    First Commit

--- RestAPIReact/settings.py
+++ RestAPIReact/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'RestAPIReact.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'frontend', 'build')],
+        'DIRS': [os.path.join(BASE_DIR,'frontend', '../frontend/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
+    os.path.join(BASE_DIR, 'frontend/build', 'static'),
     os.path.join(BASE_DIR, "media"),
 ]
 
