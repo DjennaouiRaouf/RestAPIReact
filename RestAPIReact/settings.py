e68440e2ccf6b13cccbf790ffcef4083c5fe9a96
djennaoui-raouf <djennaoui.raouf@gmail.com>
2023-04-29 21:34:53 +0200

    First Commit

--- RestAPIReact/settings.py
+++ RestAPIReact/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'RestAPIReact.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'frontend', '../frontend/build')],
+        'DIRS': [os.path.join(BASE_DIR,'frontend', 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
