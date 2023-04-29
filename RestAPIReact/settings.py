7c41b5cff02b49b2e643f5c6c36e9c44379fb3bc
djennaoui-raouf <djennaoui.raouf@gmail.com>
2023-04-29 21:26:53 +0200

    First Commit

--- RestAPIReact/settings.py
+++ RestAPIReact/settings.py
@@ -171,15 +171,15 @@ JAZZMIN_SETTINGS = {
 }
 
 
-'''
 CSRF_TRUSTED_ORIGINS = [
     'https://djennaouiraouf.up.railway.app',
 ]
 CORS_ORIGIN_WHITELIST = [
-    #'https://djennaouiraouf.up.railway.app',
+    'https://djennaouiraouf.up.railway.app',
 ]
 
 '''
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+'''
